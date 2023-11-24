89213366bc3a2f12ff7b97401472abadcd623485
gpffh20 <eunbin584836@gmail.com>
2023-11-24 16:07:03 +0900

    feat: PostViewSet에 권한 customize

--- webapp/post/views/postViewSet.py
+++ webapp/post/views/postViewSet.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from post.models import Post
@@ -7,3 +8,13 @@ from post.serializers.postSerializer import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+    ]
+    def get_permissions(self):
+        permission_classes = []
+
+        if self.action in ['update', 'retrieve']:
+            permission_classes = [IsAdminUser]
+
+        return [permission() for permission in permission_classes]
