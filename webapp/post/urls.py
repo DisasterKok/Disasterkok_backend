06702b03f8008a3a647d34da96b45e19c88637af
gpffh20 <eunbin584836@gmail.com>
2023-12-02 14:19:27 +0900

    feat: postimage viewset 생성

--- webapp/post/urls.py
+++ webapp/post/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from post.views.postViewSet import PostViewSet
+from post.views.postImageViewSet import PostImageViewSet
 
 router = DefaultRouter()
 router.register('post', PostViewSet)
+router.register('postimage', PostImageViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -11,3 +13,6 @@ urlpatterns = [
 
 # post-list: /posts/post/ ({'get': 'list', 'post': 'create'})
 # post-detail: /posts/post/<pk>/ ({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
+
+# postimage-list: /posts/postimage/ ({'get': 'list', 'post': 'create'})
+# postimage-detail: /posts/postimage/<pk>/ ({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
