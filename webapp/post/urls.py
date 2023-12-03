6710266ae485219c54d0f1ee5574783b317bc982
gpffh20 <eunbin584836@gmail.com>
2023-12-04 03:02:29 +0900

    feat: 좋아요 url설정

--- webapp/post/urls.py
+++ webapp/post/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from post.views.postViewSet import PostViewSet
+from post.views.postLikeAPIView import PostLikeAPIView
 
 router = DefaultRouter()
 router.register(r'post', PostViewSet)
 
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('post/<int:post_id>/likes/', PostLikeAPIView.as_view(), name='likes'),
 ]
 
 # post-list: /posts/post/ ({'get': 'list', 'post': 'create'})
