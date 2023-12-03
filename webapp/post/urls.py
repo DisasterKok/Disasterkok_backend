adfbfe857eaf432f6905ca503df2d46cc760c101
gpffh20 <eunbin584836@gmail.com>
2023-12-04 01:44:44 +0900

    refactor: 불필요한 postimage 관련 주석 삭제

--- webapp/post/urls.py
+++ webapp/post/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from post.views.postViewSet import PostViewSet
-# from post.views.postImageViewSet import PostImageViewSet
 
 router = DefaultRouter()
 router.register(r'post', PostViewSet)
-# router.register('postimage', PostImageViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -13,6 +11,3 @@ urlpatterns = [
 
 # post-list: /posts/post/ ({'get': 'list', 'post': 'create'})
 # post-detail: /posts/post/<pk>/ ({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
-
-# postimage-list: /posts/postimage/ ({'get': 'list', 'post': 'create'})
-# postimage-detail: /posts/postimage/<pk>/ ({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
