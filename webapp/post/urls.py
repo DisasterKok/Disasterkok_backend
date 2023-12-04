8537b5c5e2737191f197c582a133ae3be345e910
gpffh20 <eunbin584836@gmail.com>
2023-12-04 17:08:41 +0900

    feat: 메인 화면에 게시물 6개 나오는거 처리

--- webapp/post/urls.py
+++ webapp/post/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from post.views.postViewSet import PostViewSet
 from post.views.postLikeAPIView import PostLikeAPIView
+from post.views.postHomeViewSet import PostHomeViewSet
 
 router = DefaultRouter()
 router.register(r'post', PostViewSet)
@@ -10,6 +11,7 @@ router.register(r'post', PostViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('post/<int:post_id>/likes/', PostLikeAPIView.as_view(), name='likes'),
+    path('top/', PostHomeViewSet.as_view({'get': 'list'}), name='top'),
 ]
 
 # post-list: /posts/post/ ({'get': 'list', 'post': 'create'})
