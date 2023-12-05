f1d96d6624bc2357722ef1641aaae19c1a0b616b
gpffh20 <eunbin584836@gmail.com>
2023-12-05 20:57:53 +0900

    fix: user정보 request.user로 변경

--- webapp/post/serializers/postSerializer.py
+++ webapp/post/serializers/postSerializer.py
@@ -2,17 +2,19 @@ from post.models import Post, PostImage
 from post.serializers.postImageSerializer import PostImageSerializer
 from rest_framework import serializers
 
-from user.models import User
-
 
 class PostSerializer(serializers.ModelSerializer):
     images = serializers.SerializerMethodField()
-    user = serializers.CharField(source='user.username')
+    user = serializers.SerializerMethodField()
     like = serializers.SerializerMethodField(read_only=True)
     view = serializers.SerializerMethodField(read_only=True)
+
     def get_images(self, obj):
         images = obj.image.all()
         return PostImageSerializer(instance=images, many=True, context=self.context).data
+
+    def get_user(self, obj):
+        return str(obj.user)
 
     def get_like(self, obj):
         return obj.like
@@ -35,11 +37,9 @@ class PostSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        username = validated_data.pop('user')['username']
-        user_instance = User.objects.get(username=username)
-        instance = Post.objects.create(user=user_instance, **validated_data)
+        validated_data['user'] = self.context['request'].user
+        posts = Post.objects.create(**validated_data)
         img_set = self.context['request'].FILES
-        for img_data in img_set.getlist('image'):
-            PostImage.objects.create(post=instance, image=img_data)
-        return instance
-
+        for img in img_set.getlist('image'):
+            PostImage.objects.create(post=posts, image=img)
+        return posts
