f53606836ece1c4d09ccf0b6f6313259c6ef91c6
gpffh20 <eunbin584836@gmail.com>
2023-12-11 04:57:56 +0900

    feat: tag 관련 모델 추가

--- webapp/post/admin/postAdmin.py
+++ webapp/post/admin/postAdmin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from post.models import Post, PostImage, PostLike
+from post.models import Post, PostImage, PostLike, PostTag
 
 admin.site.register(Post)
 admin.site.register(PostImage)
 admin.site.register(PostLike)
+admin.site.register(PostTag)
