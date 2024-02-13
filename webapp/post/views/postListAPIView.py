a094dd46906b778650680ff0bc60a7da3dc810ef
gpffh20 <eunbin584836@gmail.com>
2024-02-13 23:31:29 +0900

    feat: 제목 및 내용으로 검색 기능 추가

--- webapp/post/views/postListAPIView.py
+++ webapp/post/views/postListAPIView.py
@@ -3,12 +3,17 @@ from post.serializers import PostSerializer
 
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework import status, pagination
+from rest_framework import status, pagination, filters
+
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     pagination_class = pagination.PageNumberPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+    # filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    # ordering_fields = ['created_at']
 
     # 게시글 작성
     def post(self, request):
